202be14651bf9933d978f7b70c501b00fa004587
Aniket Mokal <aniketmokal29@gmail.com>
2026-03-14 23:44:54 +0530

    3.1 & 3.2 Setup Project & Hibernate ORM Basic Database Config

---
+++ jpa/src/main/java/com/mokal/jpa/entity/Product.java
@@ -0,0 +1,46 @@
+package com.mokal.jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(
+        name = "product_table",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "title_price_unique", columnNames = {"title","price"})
+        },
+        indexes = {
+                @Index(name = "sku_index", columnList = "sku")
+        }
+)
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String sku;
+
+    private String title;
+
+    private BigDecimal price;
+    private Integer quantity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ jpa/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO jpadatabase.product_table (price,quantity, sku, title)
+VALUES
+(12.5,4,'parle723','Parle Biscute'),
+(13.3, 2,'pessi34', 'Peppsi'),
+(55.3,5, 'gg565', 'G G');
