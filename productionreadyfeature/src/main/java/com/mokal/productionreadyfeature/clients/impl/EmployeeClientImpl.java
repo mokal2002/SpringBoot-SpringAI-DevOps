37a3128216566c795f59904086929847ff693d7f
Aniket Mokal <aniketmokal29@gmail.com>
2026-04-15 22:54:52 +0530

    logs

--- productionreadyfeature/src/main/java/com/mokal/productionreadyfeature/clients/impl/EmployeeClientImpl.java
+++ productionreadyfeature/src/main/java/com/mokal/productionreadyfeature/clients/impl/EmployeeClientImpl.java
@@ -5,6 +5,8 @@ import com.mokal.productionreadyfeature.clients.EmployeeClient;
 import com.mokal.productionreadyfeature.dto.EmployeeDTO;
 import com.mokal.productionreadyfeature.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
@@ -19,47 +21,72 @@ public class EmployeeClientImpl implements EmployeeClient {
     private final RestClient restClient;
 
 
+    Logger log = LoggerFactory.getLogger(EmployeeClientImpl.class);
+
     @Override
     public List<EmployeeDTO> getAllEmployees() {
+        log.trace("Trying to retrieve all employees in getAllEmployees");
+        try{
+            ApiResponse<List<EmployeeDTO>> employees = restClient.get()
+                    .uri("/emp")
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<>() {});
+            log.debug("Successfully retrieve data from getAllEmployess()");
+            assert employees != null;
+            log.trace("Retrieve  employee List from getAllEmployees {}", employees.getData());
 
-        List<EmployeeDTO> employees = restClient.get()
-                .uri("/emp")
-                .retrieve()
-                .body(new ParameterizedTypeReference<List<EmployeeDTO>>() {});
+            return employees.getData();
+        }catch (Exception e){
+            log.error("Exception Occurred in getAllEmployees() {}", String.valueOf(e));
+            throw new RuntimeException(e);
+        }
 
-        return employees;
     }
 
     @Override
     public EmployeeDTO getEmpById(Long empId) {
         try {
+            log.info("Connecting to Data");
             ApiResponse<EmployeeDTO> employeeDTOApiResponse = restClient.get()
                     .uri("emp/{empId}",empId)
                     .retrieve()
-                    .body(new ParameterizedTypeReference<ApiResponse<EmployeeDTO>>() {
+                    .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                        log.error(new String(res.getBody().readAllBytes()));
+                        throw new ResourceNotFoundException("could not create the emp");
+                    })
+                    .body(new ParameterizedTypeReference<>() {
                     });
+            assert employeeDTOApiResponse != null;
             return employeeDTOApiResponse.getData();
 
         }catch (Exception e){
+            log.error("Exception Occurred in getEmpById() {}", String.valueOf(e));
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public EmployeeDTO createNewEmp(EmployeeDTO employeeDTO) {
+        log.trace("trying to create Employee with Given Information {}",employeeDTO);
         try {
             ApiResponse<EmployeeDTO> employeeDTOApiResponse = restClient.post()
                     .uri("emp")
                     .body(employeeDTO)
                     .retrieve()
                     .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                        log.debug("4xxClient error occured during createNewEmployee()");
+                        log.error(new String(res.getBody().readAllBytes()));
                         throw new ResourceNotFoundException("could not create the emp");
                     })
                     .body(new ParameterizedTypeReference<>() {
                     });
+            log.trace("Succsesfully created a new employeee: {}",employeeDTOApiResponse.getData());
+            assert employeeDTOApiResponse != null;
             return employeeDTOApiResponse.getData();
 
         }catch (Exception e){
+            log.error("Exception Occurred in createNewEmp() {}", String.valueOf(e));
+
             throw new RuntimeException(e);
         }
     }
