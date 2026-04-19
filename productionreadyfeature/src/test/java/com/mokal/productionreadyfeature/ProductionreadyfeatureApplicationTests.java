c2765249987d1ac64f7c660cb1d6af39f9e363d6
Aniket Mokal <aniketmokal29@gmail.com>
2026-04-19 15:59:05 +0530

    4.7

--- productionreadyfeature/src/test/java/com/mokal/productionreadyfeature/ProductionreadyfeatureApplicationTests.java
+++ productionreadyfeature/src/test/java/com/mokal/productionreadyfeature/ProductionreadyfeatureApplicationTests.java
@@ -12,25 +12,25 @@ import java.util.List;
 
 @SpringBootTest
 class ProductionreadyfeatureApplicationTests {
-	@Autowired
-	private EmployeeClient employeeClient;
-
-	@Test
-	void getAllEmployess(){
-		List<EmployeeDTO> employeeDTOS = employeeClient.getAllEmployees();
-		System.out.println(employeeDTOS);
-	}
-
-	@Test
-	void getEmpById(){
-		EmployeeDTO employeeDTO = employeeClient.getEmpById(1L);
-		System.out.println(employeeDTO);
-	}
-
-	@Test
-	void createEmp(){
-		EmployeeDTO employeeDTO= new EmployeeDTO(null,"Aniket","aniket@gmail.com",2, "USER", LocalDate.of(2020,12,02), true);
-		EmployeeDTO savedEmp= employeeClient.createNewEmp(employeeDTO);
-		System.out.println(savedEmp);
-	}
+//	@Autowired
+//	private EmployeeClient employeeClient;
+//
+//	@Test
+//	void getAllEmployess(){
+//		List<EmployeeDTO> employeeDTOS = employeeClient.getAllEmployees();
+//		System.out.println(employeeDTOS);
+//	}
+//
+//	@Test
+//	void getEmpById(){
+//		EmployeeDTO employeeDTO = employeeClient.getEmpById(1L);
+//		System.out.println(employeeDTO);
+//	}
+//
+//	@Test
+//	void createEmp(){
+//		EmployeeDTO employeeDTO= new EmployeeDTO(null,"Aniket","aniket@gmail.com",2, "USER", LocalDate.of(2020,12,02), true);
+//		EmployeeDTO savedEmp= employeeClient.createNewEmp(employeeDTO);
+//		System.out.println(savedEmp);
+//	}
 }
