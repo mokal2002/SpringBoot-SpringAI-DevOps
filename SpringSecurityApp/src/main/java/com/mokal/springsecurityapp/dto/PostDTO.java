1ae80b99ef8dd09eb5b074b4742a9b9f09d09d6e
Aniket Mokal <aniketmokal29@gmail.com>
2026-05-15 00:12:28 +0530

    push delete

--- SpringSecurityApp/src/main/java/com/mokal/springsecurityapp/dto/PostDTO.java
+++ SpringSecurityApp/src/main/java/com/mokal/springsecurityapp/dto/PostDTO.java
@@ -40,11 +40,3 @@ public class PostDTO {
 
 
 
-
-//#Oauth Clients Config
-//spring.security.oauth2.client.registration.google.client-id=564405635327-m3icmge10dvin5knqt8v740n81kqunru.apps.googleusercontent.com
-//spring.security.oauth2.client.registration.google.client-secret=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
-//spring.security.oauth2.client.registration.github.client-id=564405635327-m3icmge10dvin5knqt8v740n81kqunru.apps.googleusercontent.com
-//spring.security.oauth2.client.registration.github.client-secret=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
-//spring.security.oauth2.client.registration.facebook.client-id=564405635327-m3icmge10dvin5knqt8v740n81kqunru.apps.googleusercontent.com
-//spring.security.oauth2.client.registration.facebook.client-secret=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
